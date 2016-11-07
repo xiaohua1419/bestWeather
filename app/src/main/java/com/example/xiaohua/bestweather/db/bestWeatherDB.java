5350966edab989013077828ba82db511725fb7bd
xiaohua <virgo_wang@msn.com>
2016-11-07 17:03:29 +0800

    "新增数据库帮助类，以及各表对应的实体类。"

---
+++ app/src/main/java/com/example/xiaohua/bestweather/db/bestWeatherDB.java
@@ -0,0 +1,20 @@
+package com.example.xiaohua.bestweather.db;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by xiaohua on 2016/11/7.
+ */
+
+public class bestWeatherDB {
+    public static final String DB_NAME = "best_weather";
+    public static final int VERSION = 1;
+    private static bestWeatherDB bestWeatherDB;
+    private SQLiteDatabase db;
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/xiaohua/bestweather/model/Province.java
@@ -0,0 +1,32 @@
+package com.example.xiaohua.bestweather.model;
+
+/**
+ * Created by xiaohua on 2016/11/7.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getProvinceName() {
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+
+}
